0b0b4e25e86589d22c93d8b105297d1c9176de5a
Karan Dayanand Wani <karanwani664@gmail.com>
2026-05-03 21:19:19 +0530

    Update

---
+++ SheriansCodingSchool/01_DSA_JavaScript/Part 1 Basics/08_Operators/01_Arithmatic/app.js
@@ -0,0 +1,48 @@
+let a = 12;
+let b = 22;
+
+console.log(a+b);
+//34
+
+console.log(a-b);
+//-10
+
+console.log(a*b);
+//264
+
+//Quotient
+console.log(a/b);
+//0.5454
+
+console.log(7/2);
+//3.5
+
+console.log(12/5);
+//2.4
+// To get answer without decimal point
+console.log(Math.floor(12/5));
+//2
+console.log(Math.floor(14/5));
+//2
+// No Round figure calculations
+
+//Remainder
+console.log(a%b);
+//12
+console.log(7%2);
+//
+
+// To get last digit use (num % 10) 
+// 10 for 1 digit, 100 for 2 and so on
+
+console.log(4580%10);
+console.log(4584%10);
+console.log(4584%100);
+
+// To get starting digit (num / 10)
+console.log(4580/10);
+console.log(Math.floor(4580/10));
+console.log(4584/10);
+console.log(Math.floor(4584/10));
+console.log(4584/100);
+console.log(Math.floor(4584/100));
---
+++ SheriansCodingSchool/01_DSA_JavaScript/Part 1 Basics/08_Operators/02_Relational/app.js
@@ -0,0 +1,32 @@
+// < > <= >= !=
+// Boolean Answer
+
+console.log(10>5);
+// True
+
+console.log(10<5);
+// False
+
+console.log(10>=5);
+// True
+console.log(10>=10);
+// True
+console.log(3>=5);
+// False
+
+console.log(10<=5);
+// True
+console.log(10<=10);
+// True
+console.log(7<=5);
+// False
+
+// Not Strict not equal to !=
+// Strict not equal to !==
+
+console.log(10!=10);
+// False
+console.log(1!=10);
+// True
+
+

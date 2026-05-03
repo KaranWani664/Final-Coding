b10834ece08ec231cef92d8d00b17a08b39fe9f8
Karan Dayanand Wani <karanwani664@gmail.com>
2026-05-03 11:59:38 +0530

    Update

---
+++ SheriansCodingSchool/01_DSA_JavaScript/Part 1 Basics/05_Type Coercion/app.js
@@ -0,0 +1,16 @@
+// Concatenation - Number becomes string
+console.log("1"+1);
+//11
+
+// Type Coercion - String becomes number
+console.log("1"-1);
+//0
+console.log("5"-1);
+//4
+console.log("4"*2);
+//8
+console.log("8"/2);
+//4
+console.log("4"%2);
+//0 remainder
+

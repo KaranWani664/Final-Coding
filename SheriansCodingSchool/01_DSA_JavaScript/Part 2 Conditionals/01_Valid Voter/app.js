90b6f876b567c92a2ba9fbe0f681423d89b25f29
Karan Dayanand Wani <karanwani664@gmail.com>
2026-05-04 21:20:29 +0530

    Update

---
+++ SheriansCodingSchool/01_DSA_JavaScript/Part 2 Conditionals/01_Valid Voter/app.js
@@ -0,0 +1,11 @@
+// Valid Voter - Who can vote
+
+let age = prompt("What is your age");
+console.log(Number(age));
+
+if (age >= 18) {
+    console.log("You can vote");
+
+} else {
+    console.log("You can't vote");
+}

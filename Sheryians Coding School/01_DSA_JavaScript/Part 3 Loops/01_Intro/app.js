2e68cb446fb40e97511156d1d35882f759f64d40
Karan Dayanand Wani <karanwani664@gmail.com>
2026-05-07 14:10:00 +0530

    Update

---
+++ Sheryians Coding School/01_DSA_JavaScript/Part 3 Loops/01_Intro/app.js
@@ -0,0 +1,20 @@
+// for 
+    // for (start; end; change){        
+    // }
+    
+    // 1 to 22
+    // for (i=1;i<23;i++) {
+    //     console.log(i);
+    // }
+
+    // 1 to 22 & for 23
+    // for (let i=1;i<23;i++) {
+    //     console.log(i);
+    // }
+    // console.log(i)
+
+// while 
+// while(condition) {code}
+
+// do while
+// do {code} while(condition);

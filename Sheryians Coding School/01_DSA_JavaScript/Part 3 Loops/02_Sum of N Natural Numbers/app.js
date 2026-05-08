33c81ffe2e135f820ccbf546ffdb868e3c32d264
Karan Dayanand Wani <karanwani664@gmail.com>
2026-05-08 15:08:34 +0530

    Update

---
+++ Sheryians Coding School/01_DSA_JavaScript/Part 3 Loops/02_Sum of N Natural Numbers/app.js
@@ -0,0 +1,14 @@
+// Sum of n Natural Numbers
+
+let n = Number(prompt("Give us your natural number"));
+console.log(n);
+
+if (isNaN(n)) {
+    console.log("Invalid Input");
+} else {
+    if (n<0) {
+        console.log("Give me positive Number");
+    } else {
+        
+    }
+}

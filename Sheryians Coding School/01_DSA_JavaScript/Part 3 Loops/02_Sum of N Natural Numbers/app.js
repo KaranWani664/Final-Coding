e5da3d74080bd4d6a9ecf218f2a9a4dc2b0394c0
Karan Dayanand Wani <karanwani664@gmail.com>
2026-05-09 09:52:37 +0530

    Update

--- Sheryians Coding School/01_DSA_JavaScript/Part 3 Loops/02_Sum of N Natural Numbers/app.js
+++ Sheryians Coding School/01_DSA_JavaScript/Part 3 Loops/02_Sum of N Natural Numbers/app.js
@@ -1,21 +1,30 @@
 // Sum of n Natural Numbers
+let pr = prompt("Give us your natural number");
 
-let n = Number(prompt("Give us your natural number"));
-console.log("Input is : " + n);
-
-let Total = 0;
-
-if (isNaN(n) || n>9) {
-    console.log("Invalid Input");
+if (pr === null) {
+    console.log("cancelled")
 } else {
-    if (n<0) {
-        console.log("Give me positive Number");
+    let n = Number(pr);
+    // If we cancel prompt function without Number() rule then it will give null value
+    console.log("Input is : " + n);
+
+    let Total = 0;
+
+    if (isNaN(n) || n>9) {
+        console.log("Invalid Input, Give me any natural number in range of 1 to 9");
     } else {
-        for (let i=1; i<=n; i++){
-            Total += i;
+        if (n<=0) {
+            console.log("Give me positive & More than Zero Number.");
+        } else {
+            for (let i=1; i<=n; i++){
+                Total += i;
+            }
+            console.log(Total);
         }
-        console.log(Total);
     }
 }
+
+// if we Press Cancel in prompt popup then it will run else block
+
 
 

ca1b1d6c5531052c5b7c134612aca6b2e0f2cc07
Karan Dayanand Wani <karanwani664@gmail.com>
2026-05-10 19:40:51 +0530

    Update

---
+++ Sheryians Coding School/01_DSA_JavaScript/Part 3 Loops/06_Break & Continue/app.js
@@ -0,0 +1,15 @@
+// Break - Stop Loop from that given point
+
+for (let i = 1; i<20; i++){
+    if (i === 11) break;
+    else console.log(i);   
+}
+
+console.log("");
+
+// Continue - Skip Current/that given Iteration & goes to next in Loop i.e. i=4
+
+for (let i = 50; i<60; i++){
+    if (i === 54) continue;
+    else console.log(i);   
+}
---
+++ Sheryians Coding School/01_DSA_JavaScript/Part 3 Loops/07_While Loop/app.js
@@ -0,0 +1,23 @@
+// While Loop
+
+// Syntax
+// start
+// while(end){
+//     code
+//     change
+// }
+
+// let i = 1;
+// while(i<23){
+//     console.log(i);
+//     i++;
+// }
+
+// When you know the iterations you use for loop
+// when you don't know iterations but you have to stop on a specific condition i.e. "Jevha i=11" then you use while loop
+
+let Input = prompt("Give anything & write 'Exit' to stop");
+
+while(Input !== 'Exit'){
+    Input = prompt("Give anything & write 'Exit' to stop");   
+}

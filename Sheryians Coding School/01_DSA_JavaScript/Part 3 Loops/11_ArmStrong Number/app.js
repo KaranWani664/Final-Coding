9e3fb5f99ebd990fd25b5581ce09f82f8498192f
Karan Dayanand Wani <karanwani664@gmail.com>
2026-05-12 21:02:29 +0530

    Update

--- Sheryians Coding School/01_DSA_JavaScript/Part 3 Loops/11_ArmStrong Number/app.js
+++ Sheryians Coding School/01_DSA_JavaScript/Part 3 Loops/11_ArmStrong Number/app.js
@@ -1,5 +1,7 @@
 // Armstrong Number
-// (1^3 + 5^3 + 3^3 = 1+125+27 = 153) (Is Armstrong)
+// 3 digit = (1^3 + 5^3 + 3^3 = 1+125+27 = 153) (Is Armstrong)
+// 4 digit = (1634 = 1^4 + 6^4 + 3^4 + 4^4 = 1 + 1296 + 81 + 256 = 1634
+// 5 digit = (9^5 + 2^5 + 7^5 + 2^5 + 7^5 =  = 92727
 
 let pr = prompt("Give us your number check Armstrong Number");
 console.log(pr);
@@ -11,30 +13,37 @@ if (pr === null || isNaN(n) || !Number.isInteger(n) || n<=0) {
     console.log("Invalid Input / Cancelled / Give me Positive Integer Number");
 } else {
     let rem = 0;
-    let CubeTotal = 0;
+    let Total = 0;
+    let CubeTotal = 1;
     let Sum = n;
-    let i = 3;
+    let i = n.length;
     let Arrayrem = [];
-    let ArrayCubeTotal = [];
+    let ArrayTotal = [];
 
     while(n>0){
         console.log("");
         rem = n%10;
         console.log("rem is: " + rem); 
         
-        CubeTotal += rem*rem*rem;
-        console.log("CubeTotal is " + CubeTotal + " when mod of " + n + " is: " + rem);    
+        while(i>0){
+            CubeTotal *= rem;
+            i--;
+        }
+
+        Total += CubeTotal;
+        CubeTotal = 1;
+        console.log("Total is " + Total + " when mod of " + n + " is: " + rem);    
         n = Math.floor(n/10);
         Arrayrem.push(rem);
-        ArrayCubeTotal.push(CubeTotal);
+        ArrayTotal.push(Total);
     }
-    // ArrayCubeTotal.reverse();
-    Arrayrem.push(...ArrayCubeTotal);
+    // ArrayTotal.reverse();
+    Arrayrem.push(...ArrayTotal);
     
     console.log("");
-    console.log(Arrayrem + " = " + CubeTotal);
+    console.log(Arrayrem + " = " + Total);
 
-    if(Sum == CubeTotal){
+    if(Sum == Total){
         console.log("This is ArmStrong Number");
     } else {
         console.log("This is not ArmStrong Number");

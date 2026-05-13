0328a7fcc5395f09a5acc289295efaccb0948541
Karan Dayanand Wani <karanwani664@gmail.com>
2026-05-13 21:24:24 +0530

    Update

--- Sheryians Coding School/01_DSA_JavaScript/Part 3 Loops/15_Easy CalC Pending/app.js
+++ Sheryians Coding School/01_DSA_JavaScript/Part 3 Loops/15_Easy CalC Pending/app.js
@@ -4,37 +4,37 @@
 // Wrong option invalid
 // Take 2 variable
 
-// let running = true;
+let running = true;
 
-// while (running) {
-//     let choice = prompt("1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n5. Exit\n\nEnter choice:");
+while (running) {
+    let choice = prompt("1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n5. Exit\n\nEnter choice:");
 
-//     if (choice === "5" || choice === null) {
-//         alert("Exiting... Tata, Bye Bye!");
-//         running = false;
-//     } 
-//     else if (choice === "1" || choice === "2" || choice === "3" || choice === "4") {
-//         let num1 = Number(prompt("Enter first number:"));
-//         let num2 = Number(prompt("Enter second number:"));
+    if (choice === "5" || choice === null) {
+        alert("Exiting... Tata, Bye Bye!");
+        running = false;
+    } 
+    else if (choice === "1" || choice === "2" || choice === "3" || choice === "4") {
+        let num1 = Number(prompt("Enter first number:"));
+        let num2 = Number(prompt("Enter second number:"));
 
-//         let result;
-//         if (choice === "1") {
-//             result = "Result: " + (num1 + num2);
-//         } else if (choice === "2") {
-//             result = "Result: " + (num1 - num2);
-//         } else if (choice === "3") {
-//             result = "Result: " + (num1 * num2);
-//         } else if (choice === "4") {
-//             if (num2 !== 0) {
-//                 result = "Result: " + (num1 / num2);
-//             } else {
-//                 result = "Error: Cannot divide by zero!";
-//             }
-//         }
-//         alert(result);
-//         console.log(result);
-//     } 
-//     else {
-//         alert("Wrong option! Invalid choice.");
-//     }
-// }
+        let result;
+        if (choice === "1") {
+            result = "Result: " + (num1 + num2);
+        } else if (choice === "2") {
+            result = "Result: " + (num1 - num2);
+        } else if (choice === "3") {
+            result = "Result: " + (num1 * num2);
+        } else if (choice === "4") {
+            if (num2 !== 0) {
+                result = "Result: " + (num1 / num2);
+            } else {
+                result = "Error: Cannot divide by zero!";
+            }
+        }
+        alert(result);
+        console.log(result);
+    } 
+    else {
+        alert("Wrong option! Invalid choice.");
+    }
+}

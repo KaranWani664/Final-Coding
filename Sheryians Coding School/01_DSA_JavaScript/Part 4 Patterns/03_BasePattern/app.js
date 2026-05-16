a2210c6a288107b810779c5748f4ba02acad503e
Karan Dayanand Wani <karanwani664@gmail.com>
2026-05-16 19:22:26 +0530

    Update

--- Sheryians Coding School/01_DSA_JavaScript/Part 4 Patterns/03_BasePattern/app.js
+++ Sheryians Coding School/01_DSA_JavaScript/Part 4 Patterns/03_BasePattern/app.js
@@ -1,10 +1,31 @@
-// Base Pattern
-// * 
-// * 
-// * 
-// * 
-// * 
-
 // process.stdout.write('Hello Home'); // No Space or next line after
 // console.log(""); // Next Line
 // process.stdout.write('Done');
+
+// Base Pattern
+
+// 1st
+// * 
+// * 
+// * 
+// * 
+// * 
+console.log("*");
+console.log("*");
+console.log("*");
+console.log("*");
+console.log("*");
+
+console.log("");
+
+// 2nd
+// * * * * *
+process.stdout.write('* ');
+process.stdout.write('* ');
+process.stdout.write('* ');
+process.stdout.write('* ');
+process.stdout.write('* ');
+
+console.log("");
+// or
+console.log("* * * * * ");
---
+++ Sheryians Coding School/01_DSA_JavaScript/Part 4 Patterns/04_Base Pattern using Loop/app.js
@@ -0,0 +1,12 @@
+// Different Creation Different Line Output on browser
+console.log("Hello");
+console.log("World");
+// Hello
+// World
+
+// Different Creation Same Line Output on Terminal (Function from Node is used)
+process.stdout.write("Hello ");
+process.stdout.write("World");
+// Hello World
+
+

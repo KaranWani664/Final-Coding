ba6f82015f25d896ad493ae7f5ed2f6dc6b4af7e
Karan Dayanand Wani <karanwani664@gmail.com>
2026-05-16 20:59:24 +0530

    Update

--- Sheryians Coding School/01_DSA_JavaScript/Part 4 Patterns/07_Right Angle Triangle/app.js
+++ Sheryians Coding School/01_DSA_JavaScript/Part 4 Patterns/07_Right Angle Triangle/app.js
@@ -26,9 +26,37 @@ console.log(" ");
 
 for (let i = 1; i<=n; i++) {
     for (let j = 1; j<=i ; j++){
-         process.stdout.write(String(j + " "));
+         process.stdout.write(j + " "); 
+         // Used Cconcatenation but we can also use String(j)
     }
     console.log(" ");
 }
 
-// 
+console.log(" ");
+
+// in js ch++ will not work
+
+// A
+// A B
+// A B C
+// A B C D
+// A B C D E
+
+for (let i = 1; i<=n; i++) {
+    for (let j = 1; j<=i ; j++){
+         process.stdout.write(String.fromCharCode(j + 64) + " ");
+    }
+    console.log(" ");
+}
+
+console.log(" ");
+
+
+for (let i = 1; i<=n; i++) {
+    let ascii = 65;
+    for (let j = 1; j<=i ; j++){
+         process.stdout.write(String.fromCharCode(ascii) + " ");
+         ascii++;
+    }
+    console.log(" ");
+}
---
+++ Sheryians Coding School/01_DSA_JavaScript/Part 4 Patterns/08_Inverted Right Angle Triangle/app.js
@@ -0,0 +1,10 @@
+// Inverted Right Angle Triangle
+
+let prompt = require('prompt-sync')();
+let n = Number(prompt("Tell me how much rows you want to print with that number "))
+
+// * * * * * 
+// * * * *
+// * * *
+// * *
+// *

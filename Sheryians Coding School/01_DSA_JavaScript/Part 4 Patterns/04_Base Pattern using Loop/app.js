6519f17d094e9b371e656b6203104a01f74122dd
Karan Dayanand Wani <karanwani664@gmail.com>
2026-05-16 19:48:53 +0530

    Update

--- Sheryians Coding School/01_DSA_JavaScript/Part 4 Patterns/04_Base Pattern using Loop/app.js
+++ Sheryians Coding School/01_DSA_JavaScript/Part 4 Patterns/04_Base Pattern using Loop/app.js
@@ -1,16 +1,10 @@
 // Base Pattern using loop & User Input
 
-// * 
-// * 
-// * 
-// * 
-// * 
+// * * * * *
 
 let prompt = require('prompt-sync')();
-let n = prompt("Tell me how much time you want to print that number ");
-process.stdout.write(n);
-console.log(" ");
-
-for (let i = 0; i<=n; i++) {
+let n = Number(prompt("Tell me how much time you want to print that number "));
+// process.stdout.write(n); // This requires only string values Number(prompt()) will give error
+for (let i = 1; i<=n; i++) {
     process.stdout.write("* ");
 }
---
+++ Sheryians Coding School/01_DSA_JavaScript/Part 4 Patterns/05_/app.js
@@ -0,0 +1,8 @@
+// if n = 5
+// then a square of * should be given with 5 rows & 5 columns
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+

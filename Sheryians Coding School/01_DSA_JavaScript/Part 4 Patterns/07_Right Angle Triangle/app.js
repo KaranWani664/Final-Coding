4db8ac339a8da98cf789794a87adf9eee1964acb
Karan Dayanand Wani <karanwani664@gmail.com>
2026-05-16 20:32:31 +0530

    Update

--- Sheryians Coding School/01_DSA_JavaScript/Part 4 Patterns/07_Right Angle Triangle/app.js
+++ Sheryians Coding School/01_DSA_JavaScript/Part 4 Patterns/07_Right Angle Triangle/app.js
@@ -1,8 +1,34 @@
 // Right Angle Triangle
 
+let prompt = require('prompt-sync')();
+let n = Number(prompt("Tell me how much rows you want to print with that number "))
+
 // * 
 // * * 
 // * * *
 // * * * * 
 // * * * * * 
 
+for (let i = 1; i<=n; i++) {
+    for (let j = 1; j<=i ; j++){
+        process.stdout.write("* ");
+    }
+    console.log(" ");
+}
+
+console.log(" ");
+
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
+
+for (let i = 1; i<=n; i++) {
+    for (let j = 1; j<=i ; j++){
+         process.stdout.write(String(j + " "));
+    }
+    console.log(" ");
+}
+
+// 

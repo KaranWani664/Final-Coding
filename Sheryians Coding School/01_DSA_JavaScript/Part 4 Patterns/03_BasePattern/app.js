4e1c43e6d7b46a09b9322593340a1eed85642025
Karan Dayanand Wani <karanwani664@gmail.com>
2026-05-16 18:31:31 +0530

    Update

---
+++ Sheryians Coding School/01_DSA_JavaScript/Part 4 Patterns/03_BasePattern/app.js
@@ -0,0 +1,10 @@
+// Base Pattern
+// * 
+// * 
+// * 
+// * 
+// * 
+
+// process.stdout.write('Hello Home'); // No Space or next line after
+// console.log(""); // Next Line
+// process.stdout.write('Done');

ec1b8c772c5be72bfd32dfef5594a9cf9a6d3dad
Karan Dayanand Wani <karanwani664@gmail.com>
2026-05-18 09:36:09 +0530

    Update

--- Sheryians Coding School/01_DSA_JavaScript/Part 4 Patterns/08_Inverted Right Angle Triangle/app.js
+++ Sheryians Coding School/01_DSA_JavaScript/Part 4 Patterns/08_Inverted Right Angle Triangle/app.js
@@ -8,3 +8,9 @@ let n = Number(prompt("Tell me how much rows you want to print with that number 
 // * * *
 // * *
 // *
+
+for (let i = 1; i<=n; i++ ) {
+    for (let j = 1; j<=i; j-- ) {
+        process.stdout.write("* ");
+    }
+}

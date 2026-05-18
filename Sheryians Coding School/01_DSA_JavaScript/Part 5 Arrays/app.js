8b76d6ab8598d20d60ec9544d3b4ddfc4585e3fa
Karan Dayanand Wani <karanwani664@gmail.com>
2026-05-18 18:01:40 +0530

    Update

---
+++ Sheryians Coding School/01_DSA_JavaScript/Part 5 Arrays/app.js
@@ -0,0 +1,18 @@
+let arr = [];
+console.log(arr);
+arr.push(100);
+console.log(arr);
+arr.push(10);
+console.log(arr);
+arr.push(20);
+console.log(arr);
+arr.push(30);
+console.log(arr);
+arr.push(false);
+console.log(arr);
+arr.push("K");
+console.log(arr);
+
+console.log("  ");
+
+console.log(arr[1]);

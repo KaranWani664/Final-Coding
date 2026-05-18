68d63f55485dbeb43d06531c2635a4d2058d8f82
Karan Dayanand Wani <karanwani664@gmail.com>
2026-05-18 16:06:00 +0530

    Update

--- Sheryians Coding School/01_DSA_JavaScript/Part 4 Patterns/08_Inverted Right Angle Triangle/app.js
+++ Sheryians Coding School/01_DSA_JavaScript/Part 4 Patterns/08_Inverted Right Angle Triangle/app.js
@@ -10,7 +10,10 @@ let n = Number(prompt("Tell me how much rows you want to print with that number 
 // *
 
 for (let i = 1; i<=n; i++ ) {
-    for (let j = 1; j<=i; j-- ) {
+    for (let j = n; j>=i; j-- ) {
         process.stdout.write("* ");
     }
+    console.log(" ");
 }
+
+// if j = 1 then j = n-i+1
---
+++ Sheryians Coding School/01_DSA_JavaScript/Part 4 Patterns/10_HW/app.js
@@ -0,0 +1,35 @@
+// HW
+
+// 1st
+//      *  
+//     * *  
+//    * * *  
+//   * * * * 
+//  * * * * *  
+
+// 2nd
+//
+
+let prompt = require('prompt-sync')();
+let n = Number(prompt("Tell me how much rows you want to print with that number "))
+
+//         *
+//       * *
+//     * * *
+//   * * * *
+// * * * * *
+
+// Inverted Right Angle Triangle as Space and Right Angle Triangle After Space
+
+for (let i = 1; i<=n; i++) {
+    // Inverted
+    for (let j = n-1; j>=i; j--){
+        process.stdout.write(" ");
+    }    
+    
+    // Right
+    for (let k = 1; k<=i; k++) {
+            process.stdout.write("* ");
+    }
+    console.log(" ");
+}

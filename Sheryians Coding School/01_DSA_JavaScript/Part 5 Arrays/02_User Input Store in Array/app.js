ae0f1f681b6ed226d7c46619e31d133fab1eaf77
Karan Dayanand Wani <karanwani664@gmail.com>
2026-05-19 13:11:36 +0530

    Update

--- Sheryians Coding School/01_DSA_JavaScript/Part 5 Arrays/02_User Input Store in Array/app.js
+++ Sheryians Coding School/01_DSA_JavaScript/Part 5 Arrays/02_User Input Store in Array/app.js
@@ -1,3 +1,42 @@
 // How to store user input in an Array
 
-// use of for loop, 5 time loop will iterate, prompt value store in a variable named as ans & 
+// use of for loop, 5 time loop will iterate, prompt value store in a variable named as ans & empty array is already created & we will puth those ans values in that array.
+
+// Bruteforce
+
+// let arr = new Array(5);
+// arr[0] = Number(prompt("Give me a number"));
+// arr[1] = Number(prompt("Give me a number"));
+// arr[2] = Number(prompt("Give me a number"));
+// arr[3] = Number(prompt("Give me a number"));
+// arr[4] = Number(prompt("Give me a number"));
+// console.log(arr)
+
+
+// let arr = new Array(5);
+// for(let i = 1; i<=5; i++){
+//     arr.push(Number(prompt("Give me a number")));
+//     console.log(arr);
+// }
+// console.log(arr);
+
+// Output - [empty × 5, 10, 20, 30, 40, 50]
+
+let arr = new Array(4);
+for (let i = 0; i < arr.length; i++) {
+    arr[i] = Number(prompt("Give me a number"));
+    console.log(arr);
+}
+// Output
+// (4) [0, empty × 3]
+// (4) [0, 0, empty × 2]
+// (4) [0, 0, 0, empty]
+// (4) [0, 0, 0, 0]
+
+// if we use <= instead of < we end up in infinite loop that breaks the fix length of array
+// Infinite
+// let arr = new Array(4);
+// for (let i = 0; i <= arr.length; i++) {
+//     arr[i] = Number(prompt("Give me a number"));
+//     console.log(arr);
+// }
---
+++ Sheryians Coding School/01_DSA_JavaScript/Part 5 Arrays/03_Some of n elements of Array/app.js
@@ -0,0 +1,4 @@
+// Some of n elements of array
+
+let arr = [10, 20, 30, 40, 50];
+

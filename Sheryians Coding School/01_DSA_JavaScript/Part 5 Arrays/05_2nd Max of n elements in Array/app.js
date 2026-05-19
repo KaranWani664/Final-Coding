b55c873c5ef949288f6e35b7f916d590e58571f7
Karan Dayanand Wani <karanwani664@gmail.com>
2026-05-19 14:39:53 +0530

    Update

--- Sheryians Coding School/01_DSA_JavaScript/Part 5 Arrays/05_2nd Max of n elements in Array/app.js
+++ Sheryians Coding School/01_DSA_JavaScript/Part 5 Arrays/05_2nd Max of n elements in Array/app.js
@@ -1,2 +1,22 @@
 // 2nd Max of n elements in Array
 
+let arr = [10, 30, 56, 43, 29, 14, 49, 60, 52, 57, 14, 58, -5];
+
+// max as 1 & compare it with 1 & 2 & keep 2 as 2nd max
+
+let max = -Infinity; // we can use Math.max(arr[0], arr[1]); also
+let secondMax = -Infinity; // we can use Math.min(arr[0], arr[1]); also
+
+for (let i = 0; i < arr.length; i++) {
+    
+    if (max<arr[i]) {
+        secondMax = max;
+        max = arr[i];
+    } else if (arr[i]!=max && secondMax<arr[i]) {
+        secondMax = arr[i];
+    }
+    
+}
+
+console.log(max);
+console.log(secondMax);
